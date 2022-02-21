83d9f4e8f0715442238f0169d22e70ac9b3cf3e8
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-02-21 21:20:36 +0530

    YT init

---
+++ app/src/main/java/com/siddydevelops/youtubeplayer/YoutubeActivity.kt
@@ -0,0 +1,29 @@
+package com.siddydevelops.youtubeplayer
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+
+class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_youtube)
+    }
+
+    override fun onInitializationSuccess(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubePlayer?,
+        p2: Boolean
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onInitializationFailure(
+        p0: YouTubePlayer.Provider?,
+        p1: YouTubeInitializationResult?
+    ) {
+        TODO("Not yet implemented")
+    }
+}

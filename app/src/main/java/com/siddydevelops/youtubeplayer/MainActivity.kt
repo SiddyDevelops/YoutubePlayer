ebef0c34045016b704e8d2704e5fd321db0534f9
Siddharth Singh <siddhartsingh2002@gmail.com>
2022-02-22 09:33:29 +0530

    Added Activity intents

--- app/src/main/java/com/siddydevelops/youtubeplayer/MainActivity.kt
+++ app/src/main/java/com/siddydevelops/youtubeplayer/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.siddydevelops.youtubeplayer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.IllegalArgumentException
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonPlaySingle.setOnClickListener(this)
+        buttonStandalone.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        val intent = when(view.id) {
+            R.id.buttonPlaySingle -> Intent(this, YoutubeActivity::class.java)
+            R.id.buttonStandalone -> Intent(this, StandaloneActivity::class.java)
+            else -> throw IllegalArgumentException("Undefined button clicked!")
+        }
+        startActivity(intent)
     }
 }
